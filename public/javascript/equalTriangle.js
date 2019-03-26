9891cddc77db9fd521930bbf7980fe8c036b1fcd
caterpillar <caterpillarc086@gmail.com>
2019-03-26 12:42:47 -0700

    the canvas is loading the previous child

--- public/javascript/equalTriangle.js
+++ public/javascript/equalTriangle.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const image = document.getElementById('imgSource');
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// const image = document.getElementById('imgSource');
 
-ctx.drawImage(image, 0, 0);
+// ctx.drawImage(image, 0, 0);
--- public/javascript/uploadPhoto.js
+++ public/javascript/uploadPhoto.js
@@ -25,23 +25,18 @@ function drop(e) {
 
   const dt = e.dataTransfer;
   const files = dt.files;
-
-   // get the previous child(get the previous img)
-  previousChild = document.getElementById("imgSource");
-  //  remove the previous child(remove the previous img)
-  dropbox.removeChild(previousChild); 
-
-  handleFiles(files);
-
-      // draw on canvas
-      const canvas = document.getElementById('canvas');
-      const ctx = canvas.getContext('2d');
-      const image = document.getElementById('imgSource');
-      console.log("draw on canvas");
-      ctx.drawImage(image, 0, 0);
   
 
+  handleFiles(files);
+  // get the previous child(get the previous img)
+  previousChild = document.getElementById("imgSource");
+
+  //  remove the previous child(remove the previous img)
+  dropbox.removeChild(previousChild);
+
+
 }
+
 
 function handleFiles(files) {
   for (let i = 0; i < files.length; i++) {
@@ -54,7 +49,12 @@ function handleFiles(files) {
     img.file = file;
     img.id = "imgSource";
     dropbox.appendChild(img); 
-    
+    // draw on canvas
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const image = document.getElementById('imgSource');
+    console.log("draw on canvas");
+    ctx.drawImage(image, 0, 0);
 
     const reader = new FileReader();
     reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
